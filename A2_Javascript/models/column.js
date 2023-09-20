e2cf01463be656be9504d260853bb2cd1fa7a8de
yueherngtang <ytan0257@student.monash.edu>
2023-09-20 14:26:02 +1000

    Addable columns. Completely revamped codes

---
+++ A2_Javascript/models/column.js
@@ -0,0 +1,46 @@
+/**
+ * Represent Event
+ * @class
+ */
+class Column {
+
+    constructor(name) {
+
+        this.id = this.generateId();
+        this.name = name;
+        this.tasks = [];
+    }
+
+
+    generateId() {
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const randomChar1 = characters[Math.floor(Math.random()*characters.length)];
+        const randomChar2 = characters[Math.floor(Math.random()*characters.length)];
+        // Random number between 0000-9999
+        const randomNumber = Math.floor(Math.random() * 10);
+        const randomNumber1 = Math.floor(Math.random() * 10);
+        const randomNumber2 = Math.floor(Math.random() * 10);
+        const randomNumber3 = Math.floor(Math.random() * 10);
+        return `E${randomChar1}${randomChar2}-${randomNumber}${randomNumber1}${randomNumber2}${randomNumber3}`;
+    }
+    
+
+    getId() {
+        return this.id;
+    }
+
+    addTasks(newTask) {
+        this.tasks.push(newTask);
+        
+    }
+
+    removeTasks(oldTask) {
+        let itemToRemove = oldTask;
+        let indexToRemove = this.tasks.indexOf(itemToRemove);
+        if (indexToRemove !== -1) {
+            this.priority.splice(indexToRemove, 1);
+            }
+    }
+}
+
+module.exports = Column;
